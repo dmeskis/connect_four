a6c500d91bae6e4af5740f8564e2153b0e407faf
dmeskis <dmeskis@gmail.com>
2018-07-16 18:15:46 -0600

    Computer can generate move

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -9,6 +9,8 @@ puts "Welcome to Connect Four"
 while b.game_over == false
   b.print_board
   user_input = t.get_user_input
-  user_input_index = t.user_input_to_index(user_input)
-  b.place_piece(user_input_index, b.player_piece)
+  user_move = t.user_input_to_index(user_input)
+  b.place_piece(user_move, b.player_piece)
+  computer_move = t.generate_computer_move
+  b.place_piece(computer_move, b.computer_piece)
 end
